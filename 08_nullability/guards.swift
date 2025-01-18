4e3e36bccb342c16b9e0a5c30120ded057b81b05
Jose Garcia <josegarcia19809@gmail.com>
2025-01-18 07:58:09 -0600

    Nullability: Guards

---
+++ 08_nullability/guards.swift
@@ -0,0 +1,12 @@
+//
+//  guards.swift
+//  Swift 2025
+//
+//  Created by Jose Garcia on 18/01/25.
+//
+
+func guards(){
+    let name: String? = "jose"
+    guard let user = name else {return}
+    print(user)
+}
