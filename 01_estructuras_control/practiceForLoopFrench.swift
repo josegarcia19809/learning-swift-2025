b6474a2b12e8a6438e7e7271a2f25b4691d32c94
Jose Garcia <josegarcia19809@gmail.com>
2025-01-21 17:43:44 -0600

    Ciclos For: diccionario French

---
+++ 01_estructuras_control/practiceForLoopFrench.swift
@@ -0,0 +1,13 @@
+//
+//  practiceForLoopFrench.swift
+//  Swift 2025
+//
+//  Created by Jose Garcia on 21/01/25.
+//
+
+func practiceForLoopFrench(){
+    let toFrench = ["one": "un", "two": "deux", "three": "trois"]
+    for (englishWord, frenchWord) in toFrench{
+        print("\(englishWord) in French is \(frenchWord)")
+    }
+}
