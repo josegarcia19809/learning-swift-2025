5da28ab55d49c80d5eb2d16980c19aca0a0d8fc4
Jose Garcia <josegarcia19809@gmail.com>
2025-01-26 19:34:34 -0600

    Arrays: animales favoritos

---
+++ 03_arrays/favouriteAnimals.swift
@@ -0,0 +1,17 @@
+//
+//  favouriteAnimals.swift
+//  Swift 2025
+//
+//  Created by Jose Garcia on 26/01/25.
+//
+
+/*
+ Create an array of your favourite animals.
+ Print out how many animals you have in your array.
+ Print out the second element in that array.
+ */
+func favouriteAnimals() {
+    let animals: Array<String> = ["dog", "tiger", "eagle"]
+    print("I have \(animals.count) animals")
+    print("The second animal is \(animals[1])")
+}
